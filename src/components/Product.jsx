b45184a8b9fefa4f2cfb43d69d06827b95d11934
Zerubabel <zerubabelyon@gmail.com>
2024-06-08 23:20:28 +0530

    added descriptive documentation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,21 +4,29 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Product = ({ prod_details, addToCart }) => {
-  const [addedToCart, setAddedToCart] = useState(false);
+  const [addedToCart, setAddedToCart] = useState(false); // For conditionally rendering the tick icon in the add to cart button
 
+  // Passes the product to th addToCart function
+  // And updates the addedToCart state
   const handleAddedToCart = () => {
-    addToCart(prod_details)
+    addToCart(prod_details);
     setAddedToCart(true);
-  }
+  };
 
   return (
     <div className="product-card">
       <img src={prod_details.img} alt="product image" />
       <p className="product__name">{prod_details.name}</p>
-      <p className="product__price">Br{prod_details.price}</p>
+      <p className="product__price">Br {prod_details.price}</p>
       <p className="product__description">{prod_details.description}</p>
       <button className="add__to-cart" onClick={handleAddedToCart}>
-        {addedToCart ? <span>Add to cart <FontAwesomeIcon icon={faCheck} /></span> : "Add to cart"}
+        {addedToCart ? (
+          <span>
+            Add to cart <FontAwesomeIcon icon={faCheck} />
+          </span>
+        ) : (
+          "Add to cart"
+        )}
       </button>
     </div>
   );
